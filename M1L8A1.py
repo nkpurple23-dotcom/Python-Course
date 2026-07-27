7cbe0f7ff79377f7d8a6aa8523459d631220cbe8
nkpurple23-dotcom <nkpurple23@gmail.com>
2026-07-27 19:28:22 -0400

    Module 1

---
+++ M1L8A1.py
@@ -0,0 +1,19 @@
+# 1) Store values in `v`, `w`, `x`, `y`, and `z`.
+v=2
+w=3
+x=4
+y=5
+z=1
+
+# 2) Calculate the expression (v + w) * x / y and store the result back in `z`.
+z=int((v+w)*x/y)
+# 3) Print the value of `z` with a message.
+print(z)
+# 4) Store a name in `name` and a number in `age`.
+name="Nikitha"
+age=12
+# 5) Check this condition using `or` and `and`:
+if name=="Nikitha" and age==12 or age>12:
+    print("Welcome")
+else:
+    print("Goodbye")
---
+++ Module 1/M1L7A3.py
@@ -0,0 +1,9 @@
+english=input("What did you get for English(scale of 1-100)? ")
+math=input("What did you get for Math(scale of 1-100)? ")
+history=input"What did you get for History(scale of 1-100)? ")
+science="What did you get for English(scale of 1-100)? ")
+music="What did you get for Music(scale of 1-100)? ")
+#Write a program to show students’ grades by entering marks for five subjects, 
+# calculating the average, and checking the grade range using membership operators in and not in. 
+# For example, use in to check whether the average is in the range 91 to 100, 81 to 90, and 
+# so on, and use not in to validate marks outside the allowed range.
