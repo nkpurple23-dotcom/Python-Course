ac432c304d55551f9b2f4c23aa107eb5ce9bcc71
nkpurple23-dotcom <nkpurple23@gmail.com>
2026-07-27 19:35:01 -0400

    Module 1

---
+++ Module 1/M1L8A2.py
@@ -0,0 +1,6 @@
+divi=int(input("What is your dividend(Enter an integer)? "))
+div=int(input("What is your divisor(Enter an integer)? "))
+if divi%div==0:
+    print(f"{divi} is divisible by {div}")
+else:
+    print(f"{divi} is not divisible by {div}")
