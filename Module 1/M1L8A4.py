be300faab11ae65a2c32bf2c25c495b50cb2f2d2
nkpurple23-dotcom <nkpurple23@gmail.com>
2026-07-27 19:44:01 -0400

    Module 1

---
+++ Module 1/M1L8A4.py
@@ -0,0 +1,7 @@
+avg=(10+20+30)/3
+if 10<=avg:
+    print("The first cyclist is slower than the average.")
+elif 20<=avg:
+    print("The second cyclist is slower than the average.")
+else:
+    print("The third cyclist is slower than the average.")
