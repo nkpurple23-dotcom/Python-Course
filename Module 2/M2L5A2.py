c58e8a95907163f1a88e7d32aae84b39f49118fd
nkpurple23-dotcom <nkpurple23@gmail.com>
2026-08-09 17:46:31 -0400

    Module 2

---
+++ Module 2/M2L5A2.py
@@ -0,0 +1,6 @@
+rows=int(input("How many rows do you want? "))
+col=int(input("How many columns do you want? "))
+for i in range(rows):
+    for i in range(col):
+        print("*", end="")
+    print()
