34f5acfecdb0cb96f18a483fc92e7ee1416a30d9
nkpurple23-dotcom <nkpurple23@gmail.com>
2026-08-25 18:45:04 -0400

    Module 4

---
+++ Module 3/Hw4.py
@@ -0,0 +1,6 @@
+valid=False
+while not valid:
+    try:
+        bill=int(input("Please enter the bill amount: "))
+        discount=int(input("Please enter discount percentage: "))
+        people=int(input(""))
---
+++ Module 4/M4L2A1.py
@@ -0,0 +1,8 @@
+tuplex=7,"food",7.4,True
+tuplex=1,2,3,4,5,6
+tuplex=tuplex+(9,)
+tuple1=1,4,50
+tuple1.count(50)
+tuplex=1,2,3,4,5,6,7
+tuplex[3:5]
+tuplex[:6]
