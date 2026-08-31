0ebcb271e6cff5c948f729373ac770595fa1a757
nkpurple23-dotcom <nkpurple23@gmail.com>
2026-08-31 19:44:19 -0400

    Module 4

--- Module 4/M4L2A2.py
+++ Module 4/M4L2A2.py
@@ -1,9 +1,15 @@
-tuplex=1,"hi", 1.6, True
-print(tuplex)
-tuplex=1,2,3,9
-print(tuplex)
-tuplex+=9,
-print(tuplex)
-print(tuplex.count(9))
-slice=tuplex[3:5]
-print(slice)
+def check(p):
+    f=len(p)-1
+    l=0
+    while l<f:
+        if p[f]==p[l]:
+            return True
+        else:
+            return False
+        l+=1
+        f-=1
+p=(1,2,3,3,2,1)
+if check(p)==True:
+    print("This tuple is a flip flop")
+else:
+    print("This tuple is not a flip flop")
