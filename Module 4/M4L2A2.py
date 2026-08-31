f4d8413b05f6efbf2c25421fa8a74ac87e2c6cf5
nkpurple23-dotcom <nkpurple23@gmail.com>
2026-08-31 19:28:53 -0400

    Module 4

---
+++ Module 4/M4L2A2.py
@@ -0,0 +1,9 @@
+tuplex=1,"hi", 1.6, True
+print(tuplex)
+tuplex=1,2,3,9
+print(tuplex)
+tuplex+=9,
+print(tuplex)
+print(tuplex.count(9))
+slice=tuplex[3:5]
+print(slice)
---
+++ Module 4/M4L3A1.py
@@ -0,0 +1,8 @@
+student_data={"id1": {"name":"John", "age":12, "gender":"male"},
+"id2": {"name":"Jane", "age":13, "gender":"female"},
+"id3": {"name":"Jane", "age":13, "gender":"female"},
+"id4": {"name":"Mary", "age":11, "gender":"female"}}
+result={}
+seen_keys=[]
+for key,value in student_data.item():
+    pass
