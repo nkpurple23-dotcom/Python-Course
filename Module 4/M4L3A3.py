61b78ebec23939b777e2cc7e8f4f05af1816dd29
nkpurple23-dotcom <nkpurple23@gmail.com>
2026-09-06 19:42:41 -0400

    Module 4

---
+++ Module 4/M4L3A3.py
@@ -0,0 +1,5 @@
+country_code={'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}
+print("India")
+print(country_code.get("India","Not Found"))
+print("Japan")
+print(country_code.get("Japan","Not Found"))
