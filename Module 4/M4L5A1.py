dcde4477200d6da6ac76cd9e0b98703a5483b6fc
nkpurple23-dotcom <nkpurple23@gmail.com>
2026-09-19 17:49:23 -0400

    Module 4

---
+++ Module 4/M4L5A1.py
@@ -0,0 +1,5 @@
+store_item_names=["pencil", "eraser", "pen", "notebook", "stapler"]
+matching_stock=[5,1,10,0,2]
+stock={}
+stock=zip(store_item_names,matching_stock)
+print(stock)
---
+++ Module 4/Quiz.py
@@ -0,0 +1,9 @@
+student_data={"id1":{"name":"Jane","score":69},
+"id2":{"name":"Mary","score":90},
+"id3":{"name":"June","score":87},
+"id4":{"name":"John","score":50},
+"id5":{"name":"Bob","score":26}}
+for key,value in student_data.items():
+    sum+=value["score"]
+avg=sum/5
+print(max(student_data)
